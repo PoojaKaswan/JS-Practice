9441535a79a0640a8177ffaaeb3e306d8074dfa4
Pooja Kaswan <gettoknowpooja@outlook.com>
2026-04-10 16:42:02 +0000

    datatypees

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";// treat all JS code as newer version 
+
+// alert("hello") // we are using nodejs, not browser
+
+console.log(3 
+    + 3) // code readability should be high
+
+console.log("Pooja")
+
+
+let name = "Pooja" // string
+let age = 20 // number
+let isLoggedIn = false // boolean
+
+/* 
+Primative Data Types:
+---------------------
+number => 2 to power 53 is the range 
+bigint
+boolean => true/false
+null => let state = null; its a object and a standalone value
+undefined =>  let state; variable is declared but value isn't given yet
+string => '' or "" // mostly used ""
+symbol => unique 
+
+
+//object
+*/
+
+console.log(typeof age) 
+console.log(typeof undefined) 
+console.log(typeof null) 
