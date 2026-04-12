f6fc1aff3a14752307fa88a5409e62dc085fa670
Pooja Kaswan <gettoknowpooja@outlook.com>
2026-04-12 14:33:46 +0000

    Math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,33 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+const otherNumber = 23.4567
+
+console.log(otherNumber.toPrecision(2));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+console.log(Number.MAX_VALUE);
+
+console.log(Math.PI);
+console.log(Math.abs(-5));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.9));
+console.log(Math.min(4, 6, 8, 1, 9));
+console.log(Math.sqrt(25));
+
+console.log((Math.random()*10) + 1);
+
+const min = 19
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
