4d83766ab7b4c5c1fc33fa03f15c79dce8d62578
Pooja Kaswan <gettoknowpooja@outlook.com>
2026-04-16 14:59:23 +0000

    accesing object values from array

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -23,7 +23,27 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 
 //const obj3 = {obj1, obj2}
-const obj4 = Object.assign({}, obj1, obj2)// {}: optional 
+//const obj4 = Object.assign({}, obj1, obj2)// {}: target object, obj1 and obj2: source objects. If we don't provide target object, then obj1 will be target object and obj2 will be source object. So, obj1 will be mutated. To avoid that, we can provide empty object as target object. 
+
+const obj3 = {...obj1, ...obj2}
 
 //console.log(obj3)
-console.log(obj1)
+//console.log(obj1)
+console.log(obj4)
+
+const objsInArr = [
+    {
+        id: 1,
+        name: "Pooja"
+    },
+    {
+        id: 2,
+        name: "Samy"
+    },
+    {
+        id: 3,
+        name: "John"
+    }
+]
+
+console.log(objsInArr[0].name);
